50812665c7572e5f12b62258bb2f7bd413e994c7
Sam Sheridan <samsheridan@Sams-Air.lan>
2021-11-07 13:18:08 -0800

    fixed string rotation

--- arrays_and_strings/string_rotation.go
+++ arrays_and_strings/string_rotation.go
@@ -4,15 +4,10 @@ import "strings"
 
 func isRotation(s1 string, s2 string) bool {
 	//given two strings and strings.Contains check if s2 is a rotation of s1
-	n := len(s1)
-
-	cut := 0
-	for i := 0; i < n; i++ {
-		if s2[0] == s1[i] {
-			cut = i
-			break
-		}
+	if len(s1) == len(s2) {
+		s1s2 := s1 + s2
+		return strings.Contains(s1s2, s2)
 	}
 
-	return strings.Contains(s2, s1[cut:])
+	return false
 }
